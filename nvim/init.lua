615f69bce8688073ce3b961284832158415e9b4b
W <william.wilkens@caesari.se>
2025-09-02 16:31:37 +0000

    removed nvim

--- nvim/init.lua
+++
@@ -1,7 +0,0 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
-
-vim.g.mapleader = " "
-
-vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true })
-vim.keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true })
--- nvim/lua/plugins/snacks.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "folke/snacks.nvim",
-  opts = {
-    scratch = { enabled = false },
-    terminal = { enabled = false },
-    scroll = { enabled = false },
-  },
-}
